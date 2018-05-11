40442447e63669833edf1111c90b58473d37d23e
Juan-Betancourt <juan.c.betancourt@hotmail.com>
2018-05-11 11:20:07 -0700

    Unable to complete via the terminal
    The most up-to-date completion of About me.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ alert('Hi ' + userName + ', thanks for stopping by!');
 console.log('The user said their name is ' + userName);
 // log to the console
 
+//========Score card=======
+var score = 0;
 
 // Ask the first guessing game question (Question 1)
 function question1() {
@@ -23,15 +25,18 @@ function question1() {
     if (answer1 === 'yes' || answer1 === 'y') {
         // right answer
         alert('You got it right!!!');
+        document.write('1. ' + userName + 'That is correct. I have a brother.');
         console.log('' + userName + ' got the correct answer to #1 (yes)');
+        score++;
     } else {
         // wrong answer
         alert('Unfortunately, I do');
+        document.write('1. ' + userName + ' Unfortunately, that was wrong I do have a brother.')
         console.log('' + userName + ' The user got the wrong answer to #1 (no)');
-    }
+    };
     // Tell user if they are wrong or right
 
-} // end function question1()
+}; // end function question1()
 
 // Next question (Question 2)
 function question2() {
@@ -40,12 +45,15 @@ function question2() {
     if (answer2 === 'yes' || answer2 === 'y') {
         //I enjoy music
         alert('You bet I love music, I am a D.J.!');
+        document.write('2. ' + userName + ' You bet I love music, I am a D.J.!');
         console.log('' + userName + ' got the correct answer.');
+        score++;
     } else { //user needs to understand music
-        alert('' + userName + ' well that is incorrect, I enjoy music.');
+        alert('Well that is incorrect, I enjoy music. ' + userName);
+        document.write('2. ' + userName + ' who doesn\'t enjoy music');
         console.log('' + userName + ' got the answer wrong');
-    }
-} // end function question2() 
+    };
+}; // end function question2() 
 
 // Next question (question 3)
 function question3() {
@@ -53,11 +61,14 @@ function question3() {
 
     if (answer3 === 'yes' || answer3 === 'y') {
         //user knows that I enjoy going on vacation
-        alert('You rock! I am looking forward to my next vacation (Jamaica)');
-        console.log('' + userName + ', knows that I enjoy vacations.');
+        alert('You rock! I am looking forward to my next vacation in (Jamaica)');
+        document.write('3. ' + userName + ' You rock! I am looking forward to my next vacation in (Jamaica)');
+        console.log(userName + ', knows that I enjoy vacations.');
+        score++;
     } else { //user hates going on vacation
-        alert('' + userName + '! no, that is not correct.');
-        console.log('' + userName + ' hates the world.');
+        alert(userName + '! no, that is not correct.');
+        document.write('3. ' + userName + '! no, that is not correct. We all love vacations');
+        console.log(userName + ' hates the world.');
     }
 } // end function question3()
 
@@ -68,9 +79,12 @@ function question4() {
     if (answer4 === 'yes' || answer4 === 'y') {
         //user found out I enjoy board games
         alert('I do enjoy board games ' + userName + '');
+        document.write('4. ' + userName + ' I do enjoy board games');
         console.log('' + userName + ', found out that I enjoy board games.');
+        score++;
     } else { //okay this user does not get out much. lol
         alert('' + userName + ', well I do enjoy board games.');
+        document.write('4. ' + userName + 'that was incorrect, I do enjoy music');
         console.log('' + userName + ' really hates the world.');
     }
 } // end function question4()
@@ -82,73 +96,84 @@ function question5() {
     if (answer5 === 'yes' || answer5 === 'y') {
         //user has had enough
         alert('sorry, I just wanted you to get to know me a little more; however I have two more questions.');
-        console.log('' + userName + ', has had enough of this.');
+        console.log('5. ' + userName + ', has had enough of this.');
     } else { //what is wrong with this user
         alert('' + userName + ', thanks for getting to know me we still have two more questions.');
-        console.log('' + userName + 'loves the question game.');
+        console.log('5. ' + userName + 'loves the question game.');
     }
 } // end function question5()
 
-// Next question (question 6)
+//Next question (question 6)
 function question6() {
     var favoriteNumber = ['1', '5'];
     var counter = 0;
-    var wrongAnswer = true;
+    var wrongAnswer = false;
 
-    do {
-        counter++;
+    while (counter < 5 && wrongAnswer === false) {
         var questionSixAnswer = prompt('What are my favorite numbers 1 through 10?');
-        for (var j = 0; j < favoriteNumber.length; j++) {
-            if (questionSixAnswer == favoriteNumber[j]) {
-                wrongAnswer = false;
-                break;
-                console.log(questionSixAnswer)
-            }
-            if (wrongAnswer === true) {
-                alert('Your guess is incorrect you have ' + counter + ' tries.')
-                break;
-                console.log(alert)
-            }
+        for (var i = 0; i < favoriteNumber.length; i++) {
+            if (questionSixAnswer === favoriteNumber[i]) {
+                wrongAnswer = true;
+            };
+            console.log(questionSixAnswer)
+        };
 
-        }
+        if (!wrongAnswer) {
+            counter++;
+            alert('Your guess is incorrect try again ' + userName);
+            console.log('wrongAnswer')
+        } else {
+            alert(userName + ' That is the correct number');
+            document.write('6. ' + userName + '' + questionSixAnswer + ' is correct');
+            counter += 5;
+            score++;
+        };
+        if (counter === 4) {
+            alert(userName + ' ,sorry my favorite numbers are 1 and 5');
+            document.write('6. ' + userName + ' ,sorry my favorite numbers are 1 and 5');
+            break;
+        };
 
-    } while (counter > 4 || wrongAnswer === false);
-    console.log(counter)
-
-    if (counter > 4) {
-        alert('You gave it four tries, unfortunately the answer is 1 and 5.')
-        console.log('' + userName + ', has the wrong answer.');
-    } else {
-        alert('It took you ' + counter + ' tries, my favorite number is 1 and 5 ' + userName + '')
-        console.log('' + userName + ', has the correct answer.');
-    }
-} // end function question6()
+    };
+}; // end function question6()
 
 function question7() {
-
     var answerArray = ['pizza', 'cheeseburger', 'hotdog'];
-    var counter = 0;
-    var notAnsweredCorrectly = true;
+    var counter = 6;
+    var didTheyGetIt = false;
 
-    do {
-        var questionSevenAnswer = prompt('What is my favorite type of foods').toLowerCase();
-        for (var i = 0; 1 < answerArray.length; i++) {
+    while (counter >= 0 && didTheyGetIt === false) {
+        var questionSevenAnswer = prompt('what is my favorite type of food').toLowerCase();
+        console.log('Before running the loop')
+        for (var i = 0; i < answerArray.length; i++) {
             if (questionSevenAnswer === answerArray[i]) {
-                break;
+                didTheyGetIt = true;
+                console.log('for loop')
             };
         };
-        counter++;
 
-    } while (counter < 6 && notAnsweredCorrectly);
+        if (!didTheyGetIt) {
+            counter--;
+            alert('That is not my favorite type of food, please try again ' + userName + '. You have ' + counter + ' tries left.');
+            console.log(userName + ' did not guess my food correctly.');
+        } else {
+            alert(questionSevenAnswer + ' is my favorite type of food ' + userName);
+            counter--;
+            score++;
+            document.write('7. ' + questionSevenAnswer + ' is my favorite type of food ' + userName);
+        };
+        if (counter === 0) {
+            alert('I\'m sorry ' + userName + 'my favorite type of food is Pizza, Cheesburgers or hotdogs');
+            document.write('7. I\'m sorry ' + userName + 'my favorite type of food is Pizza, Cheesburgers or hotdogs');
+            break;
+        };
+    };
+}; // end function for question7()
 
-    if (notAnsweredCorrectly) {
-        alert('It took you six tries but you failed')
-        console.log('' + userName + ', has the wrong answer.');
-    } else {
-        alert('It took you ' + counter + ' tries, my favorite food is Pizza, Cheeseburgers, and Hotdogs')
-        console.log('' + userName + ', has the correct answer.');
-    }
-} // end function for question7()
+function displayCorrectAnswers() {
+    alert(userName + ' out of the six question you got ' + score + ' correct.');
+    document.write(userName + ' out of the 6 question you got ' + score + ' correct.');
+};
 
 question1();
 question2();
@@ -157,3 +182,4 @@ question4();
 question5();
 question6();
 question7();
+displayCorrectAnswers();
