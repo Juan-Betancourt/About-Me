cc4c94252a55d0399bf77b1dca7f08331af10089
Juan-Betancourt <juan.c.betancourt@hotmail.com>
2018-05-17 10:35:31 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -13,26 +13,19 @@ var score = 0;
 
 // Ask the first guessing game question (Question 1)
 function question1() {
-    var answer1 = prompt('Do you think I have any siblings?').toLowerCase();
-    // user guess "yes/no or "Y/N"
-
-    // if (condition is true ){
-    //     do this stuff
-    // } else {
-    //     do this stuff
-    // }
+    var answer1 = prompt('Do you think I have any siblings? Please answer with yes (y) or no (n)').toLowerCase();
 
     if (answer1 === 'yes' || answer1 === 'y') {
         // right answer
         alert('You got it right!!!');
-        document.write('<br><center>1. ' + userName + ' that is correct. I have a brother.</center></br>');
-        console.log('' + userName + ' got the correct answer to #1 (yes)');
+        document.write('<center><br>1. ' + userName + ' That is correct. I have a brother. </br></center>');
+        console.log(userName + ' got the correct answer to #1 (yes)');
         score++;
     } else {
         // wrong answer
         alert('Unfortunately, I do');
-        document.write('<br><center>1. ' + userName + ' unfortunately, that was wrong I do have a brother.</center></br>')
-        console.log('' + userName + ' The user got the wrong answer to #1 (no)');
+        document.write('<center><br>1. ' + userName + ' Unfortunately, that was wrong I do have a brother. </br></center>')
+        console.log(userName + ' got the wrong answer to #1 (no)');
     };
     // Tell user if they are wrong or right
 
@@ -40,79 +33,77 @@ function question1() {
 
 // Next question (Question 2)
 function question2() {
-    var answer2 = prompt('Do I like music?').toLowerCase();
+    var answer2 = prompt('Do you think I enjoy music? Please answer with yes (y) or no (n)').toLowerCase();
 
     if (answer2 === 'yes' || answer2 === 'y') {
         //I enjoy music
         alert('You bet I love music, I am a D.J.!');
-        document.write('<br><center>2. ' + userName + ' you bet I love music, I am a D.J.!</center></br>');
-        console.log('' + userName + ' got the correct answer.');
+        document.write('<center><br>2. ' + userName + ' You bet I love music, I am a D.J.! </br></center>');
+        console.log(userName + ' knows I enjoy music.');
         score++;
     } else { //user needs to understand music
-        alert('Well that is incorrect, I enjoy music. ' + userName);
-        document.write('<br><center>2. ' + userName + ' who doesn\'t enjoy music</center></br>');
-        console.log('' + userName + ' got the answer wrong');
+        alert('No that is not correct, I enjoy music and I am also a D.J.. ' + userName);
+        document.write('<center><br>2. ' + userName + ' who doesn\'t enjoy music </br></center>');
+        console.log(userName + ' thinks I hate music.');
     };
 }; // end function question2() 
 
 // Next question (question 3)
 function question3() {
-    var answer3 = prompt('Do I enjoy vacations?').toLowerCase();
+    var answer3 = prompt('Do you think I served in the Air Force? Please answer with yes (y) or no (n)').toLowerCase();
 
     if (answer3 === 'yes' || answer3 === 'y') {
-        //user knows that I enjoy going on vacation
-        alert('You rock! I am looking forward to my next vacation in (Jamaica)');
-        document.write('<br><center>3. ' + userName + ' you rock! I am looking forward to my next vacation in (Jamaica)</center></br>');
-        console.log(userName + ', knows that I enjoy vacations.');
+        //user knows that I served in the Air Force
+        alert('You rock! I served in the Air Force for over 10 years.');
+        document.write('<center><br>3. ' + userName + ' You rock! I served in the Air Force for over 10 years. </br></center>');
+        console.log(userName + ', knows that I served in the armed forces.');
         score++;
-    } else { //user hates going on vacation
+    } else { //user thinks that I did not serve my country
         alert(userName + '! no, that is not correct.');
-        document.write('<br><center>3. ' + userName + '! no, that is not correct. We all love vacations</center></br>');
-        console.log(userName + ' hates the world.');
+        document.write('<center><br>3. ' + userName + '! no, that is not correct. I have served over 10 years in the Air Force. </br></center>');
+        console.log(userName + ' does not think that I served my country.');
     }
 } // end function question3()
 
 // Next question (question 4)
 function question4() {
-    var answer4 = prompt('Do I enjoy playing board games?').toLowerCase();
+    var answer4 = prompt('Do I enjoy playing board games? Please answer with yes (y) or no (n)').toLowerCase();
 
     if (answer4 === 'yes' || answer4 === 'y') {
         //user found out I enjoy board games
         alert('I do enjoy board games ' + userName + '');
-        document.write('<br><center>4. ' + userName + ' I do enjoy board games</center></br>');
-        console.log('' + userName + ', found out that I enjoy board games.');
+        document.write('<center><br>4. ' + userName + ' I do enjoy board games. </br></center>');
+        console.log(userName + ', found out that I enjoy board games.');
         score++;
     } else { //okay this user does not get out much. lol
-        alert('' + userName + ', well I do enjoy board games.');
-        document.write('<br><center>4. ' + userName + ' that was incorrect, I do enjoy music</center></br>');
-        console.log('' + userName + ' really hates the world.');
+        alert(userName + ', well wrong. I do enjoy board games.');
+        document.write('<center><br>4. ' + userName + 'that was incorrect, I do enjoy board games. </br></center>');
+        console.log(userName + ' really hates the world.');
     }
 } // end function question4()
 
 // Next question (question 5)
 function question5() {
-    var answer5 = prompt('I am guessing that you had enough of these questions?').toLowerCase();
+    var answer5 = prompt('I am guessing that you had enough of these questions? Please answer with yes (y) or no (n)').toLowerCase();
 
     if (answer5 === 'yes' || answer5 === 'y') {
         //user has had enough
-        alert('sorry, I just wanted you to get to know me a little more; however I have two more questions.');
-        console.log('5. ' + userName + ', has had enough of this.');
+        alert('sorry, I just wanted you to get to know me a little more; however, I have two more questions.');
+        console.log(userName + ', has had enough of this.');
     } else { //what is wrong with this user
-        alert('' + userName + ', thanks for getting to know me we still have two more questions.');
-        console.log('5. ' + userName + 'loves the question game.');
+        alert(userName + ', thanks for getting to know me we still have two more questions.');
+        console.log(userName + 'loves the question game.');
     }
-    document.write('<br><center>5. We are skiiping question five</center></br>')
 } // end function question5()
-
 
 //Next question (question 6)
 function question6() {
-    var favoriteNumber = ['1', '5'];
+    var favoriteNumber = ['1', '11'];
     var counter = 0;
     var wrongAnswer = false;
 
     while (counter < 5 && wrongAnswer === false) {
-        var questionSixAnswer = prompt('I have two favorite numbers 1 through 10 let\'s see if you can guess one?');
+        var questionSixAnswer = prompt('I have two favorite numbers 1 through 20 can you guess one? Please utilize a numeric number');
         for (var i = 0; i < favoriteNumber.length; i++) {
             if (questionSixAnswer === favoriteNumber[i]) {
                 wrongAnswer = true;
@@ -123,16 +114,16 @@ function question6() {
         if (!wrongAnswer) {
             counter++;
             alert('Your guess is incorrect try again ' + userName);
-            console.log('wrongAnswer')
+            console.log('wrong answer')
         } else {
             alert(userName + ' That is the correct number');
-            document.write('<br><center>6. ' + userName + '' + questionSixAnswer + ' is correct</center></br>');
+            document.write('<center><br>5. ' + userName + '' + questionSixAnswer + ' is correct. </br></center>');
             counter += 5;
             score++;
         };
         if (counter === 4) {
-            alert(userName + ' ,sorry my favorite numbers are 1 and 5');
-            document.write('<br><center>6. ' + userName + ' ,sorry my favorite numbers are 1 and 5</center></br>');
+            alert(userName + ' ,sorry my favorite numbers are 1 and 11');
+            document.write('<center><br>5. ' + userName + ' ,sorry my favorite numbers are 1 and 11. </br></center>');
             break;
         };
 
@@ -162,19 +153,19 @@ function question7() {
             alert(questionSevenAnswer + ' is my favorite type of food ' + userName);
             counter--;
             score++;
-            document.write('<br><center>7. ' + questionSevenAnswer + ' is my favorite type of food ' + userName + '</center></br>');
+            document.write('<center><br>6. ' + questionSevenAnswer + ' is my favorite type of food. ' + userName + '</br></center>');
         };
-        if (counter === 0) {
+        if (counter === (0)) {
             alert('I\'m sorry ' + userName + ' my favorite type of food is Pizza, Cheesburgers or hotdogs');
-            document.write('<br><center>7. I\'m sorry ' + userName + ' my favorite type of food is Pizza, Cheesburgers or hotdogs </center></br>');
+            document.write('<center><br>6. I\'m sorry ' + userName + ' my favorite type of food is Pizza, Cheesburgers or hotdogs. </br></center>');
             break;
         };
     };
 }; // end function for question7()
 
 function displayCorrectAnswers() {
-    alert(userName + ' out of the 6 questions you got ' + score + ' correct.');
-    document.write('<br><center>' + userName + ' out of the 6 question you got ' + score + ' correct.</center></br>');
+    alert(userName + ' out of the six question you got ' + score + ' correct.');
+    document.write('<center><br> ' + userName + ' out of the 6 question you got ' + score + ' correct. </br></center>');
 };
 
 question1();
